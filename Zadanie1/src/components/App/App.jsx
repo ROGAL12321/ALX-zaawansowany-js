63e05b4bb943e0a8042db34e9a08e698ac8604f8
Damian Rogulski <rogaldj@gmail.com>
2022-03-20 11:36:36 +0100

    Basic React App

---
+++ Zadanie1/src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const App = <div>
+  <h1>Todo list</h1>
+  <form>
+    <input type="text" placeholder="Write todo"/>
+    <button type="submit">send todo</button>
+  </form>
+</div>;
