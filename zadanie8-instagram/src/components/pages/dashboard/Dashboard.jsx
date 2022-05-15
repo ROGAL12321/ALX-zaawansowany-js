c48b407f6e6b4b58f635786c0f8070aec12c41c9
Damian Rogulski <rogaldj@gmail.com>
2022-05-15 13:05:59 +0200

    feat: handle like

--- zadanie8-instagram/src/components/pages/dashboard/Dashboard.jsx
+++ zadanie8-instagram/src/components/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Main from 'components/layouts/main/Main';
 import { RestrictedRoute } from 'utils/AuthorizationRoutes';
-import { get } from 'services/firebase';
+import { update, get } from 'services/firebase';
 
 import styles from './style.module.css';
 
@@ -14,10 +14,20 @@ function Dashboard() {
     });
   }, []);
 
-  console.log(posts);
+  const handleLike = (post) => {
+    update(`posts/${post.id}`, {
+      ...post,
+      likes: post.likes + 1,
+    }).then(() => {
+      const copiedPostArray = [...posts];
+      const selectedPostIndex = posts.findIndex(
+        (frontPost) => frontPost.id === post.id
+      );
+      copiedPostArray[selectedPostIndex].likes = post.likes + 1;
 
-  // post.author?.name
-  // post.author?.avatar
+      setPosts(copiedPostArray);
+    });
+  };
 
   return (
     <RestrictedRoute>
@@ -31,6 +41,15 @@ function Dashboard() {
               </div>
               <img src={post.image} alt="post" />
               <div className={styles.descriptionContainer}>
+                <div className={styles.likesContainer}>
+                  <button onClick={() => handleLike(post)}>
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Ei-like.svg/1200px-Ei-like.svg.png"
+                      alt="Like"
+                    />
+                    Like ({post.likes})
+                  </button>
+                </div>
                 <p className={styles.title}>{post.title}</p>
                 <p className={styles.description}>{post.description}</p>
               </div>
